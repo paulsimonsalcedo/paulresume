6890d1a7f2ae16e194928ac8c7238e9e37b6d978
paulsimonsalcedo <85142484+paulsimonsalcedo@users.noreply.github.com>
2022-05-22 20:08:58 +0800

    navupdate

--- include.js
+++ include.js
@@ -9,23 +9,20 @@ window.addEventListener("scroll", function(){
 			reveals.classList.toggle("active", window.scrollY > 30)
 
 
-			var scroll3 = document.getElementById("click_scroll3");
-			var scroll4 = document.getElementById("click_scroll4");
-			var scroll5 = document.getElementById("click_scroll5");
-
-
 		});	
 	
 	$("#click_scroll").click(function() {
 	    	$('html,body').animate({
 	        scrollTop: $("#home").offset().top},
 	        'slow' > 2);
+	    
 
 	});
 	$("#click_scroll2").click(function() {
 	    	$('html,body').animate({
 	        scrollTop: $("#abt").offset().top},
 	        'slow' > 2);
+	    
 	       
 	});
 	$("#click_scroll3").click(function() {
@@ -33,20 +30,39 @@ window.addEventListener("scroll", function(){
 	        scrollTop: $("#resume").offset().top},
 	        'slow' > 2);
 	       
+	       
 	});
 	$("#click_scroll4").click(function() {
 	    	$('html,body').animate({
 	        scrollTop: $("#techstack").offset().top},
 	        'slow' > 2);
+	      
 	         
 	});
 	$("#click_scroll5").click(function() {
 	    	$('html,body').animate({
 	        scrollTop: $("#RecentProject").offset().top},
 	        'slow' > 2);
+	       
 	  
 	});
 
 /****** IN JQUERY *****
  * $(this).toggleClass(ClassName);
  * *******************/
+
+
+ let item = document.querySelectorAll(".links");
+ let sec = document.querySelectorAll("section");
+
+
+  window.addEventListener("scroll", activeMenu);
+  activeMenu();
+
+ function activeMenu(){
+ 	let len = sec.length;
+ 	while(--len && window.scrollY + 97 < sec[len].offsetTop){
+ 	}
+ 		item.forEach(ltx => ltx.classList.remove("active"));
+ 		item[len].classList.add("active");
+ }
